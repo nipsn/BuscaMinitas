4c3b5c89aea8d6c062b79bf0d91f545e7b49c488
oscar.nydza <o.nydza.2017@alumnos.urjc.es>
2022-09-13 17:08:45 +0200

    Testing print options

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,22 @@
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+//    val mymultiarr= Array.ofDim[(Int, Int)](4, 4)
+    val mymultiarr= Array.ofDim[Int](8, 8)
+
+    //Assigning the values
+    mymultiarr(0)(0) = 2
+    mymultiarr(0)(1) = 7
+    mymultiarr(1)(0) = 3
+    mymultiarr(1)(1) = 4
+
+    for(i<-0 to 1) {
+      for(j<-0 to 1) {
+        print("[" + mymultiarr(i)(j) + "]")
+      }
+      println()
+    }
+
+
+    print(mymultiarr.map("[" + _.mkString("][") + "]").mkString("\n"))
   }
 }
