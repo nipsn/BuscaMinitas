3d63b9f80b68a22a193676d8c02e982cfb8c5ea8
oscar.nydza <oscar.nydza@pm.me>
2022-09-30 19:23:56 +0200

    Random bomb generation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,21 +3,7 @@ import MineSweeper._
 object Main {
   def main(args: Array[String]): Unit = {
 
-    val grid: Grid = (5,5).emptyGrid
-
-    println(grid.mkString)
-    println("\n")
-
-    val gridTransformed = grid.map(row =>
-      row.map(_ =>Cell(Numbered(2))))
-      .modify(0,1)(Cell(Numbered(3)).makeVisible)
-      .modify(0,0)(Cell(Empty).makeVisible)
-      .modify(1,0)(Cell(Empty).makeVisible)
-      .modify(2,0)(Cell(Empty))
-      .modify(3,0)(Cell(Empty))
-      .modify(4,0)(Cell(Empty))
-
-    println(gridTransformed.mkString)
+    println((5, 5).emptyGridWithBombs(2).mkString)
 
   }
 }
--- src/main/scala/MineSweeper/package.scala
+++ src/main/scala/MineSweeper/package.scala
@@ -5,12 +5,30 @@ package object MineSweeper {
   implicit class EmptyGridFromDim(tuple: (Int, Int)) {
     def emptyGrid: Grid = Array.ofDim[Cell](tuple._1, tuple._2)
       .map(row => row.map(_ => Cell(Empty)))
+
+    def emptyGridWithBombs(nBombs: Int): Grid = {
+      generateRandomBombs(tuple, nBombs)
+        .foldRight(emptyGrid)((coords, myGrid) => myGrid.modify(coords)(Cell(visible = true, tagged = false, Bomb)))
+    }
+
+    private def generateRandomBombs(gridSize: (Int, Int), nBombs: Int): List[(Int, Int)] = {
+      val (coordX, coordY) = gridSize
+      val r = scala.util.Random
+      // TODO: bombs can generate in same coords multiple times (check nBombs == set.size)
+      (1 to nBombs).foldRight(Set[(Int, Int)]())((_, mySet) => mySet.incl((r.nextInt(coordX), r.nextInt(coordY)))).toList
+    }
   }
 
   /* Matrix operation syntax */
   implicit class MatrixSyntax(grid: Grid) {
     def apply(x: Int, y: Int): Cell = grid(x)(y)
+
     def modify(x: Int, y: Int)(cell: Cell): Grid = grid.updated(x, grid(x).updated(y, cell))
+
+    def modify(coords: (Int, Int))(cell: Cell): Grid = {
+      val (x, y) = coords
+      grid.updated(x, grid(x).updated(y, cell))
+    }
     def mkString: String = grid.map(row => row.map(cell => cell.toString).mkString("[", "][", "]")).mkString("\n")
   }
 
